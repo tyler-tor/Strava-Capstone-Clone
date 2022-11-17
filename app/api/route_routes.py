75ac4c2717daca66d4083367fab3973b927833b2
tyler <dtctorrero@icloud.com>
2022-11-16 20:20:41 -0800

    started backend routes/testing, have to reload due to vs code issue

--- app/api/route_routes.py
+++ app/api/route_routes.py
@@ -1,8 +1,20 @@
-from flask import Blueprint, jsonify
-from flask_login import login_required
-from app.models import User, Route
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, current_user
+from app.models import User, Route, db
+from ..forms.route_form import CreateRouteForm
 
 route_routes = Blueprint('routes', __name__)
+
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
 
 
 @route_routes.route('/')
@@ -13,6 +25,30 @@ def all_routes():
     """
     routes = Route.query.all()
     return {'routes': [route.to_dict() for route in routes]}
+
+
+@route_routes.route('/', methods=['POST'])
+@login_required
+def add_route():
+    form = CreateRouteForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        route = Route(
+            user_id=current_user.id,
+            title=form.data['title'],
+            description=form.data['description'],
+            image_url=form.data['image_url'],
+            starting_point=form.data['starting_point'],
+            ending_point=form.data['ending_point'],
+            distance=form.data['distance']
+        )
+        db.session.add(route)
+        db.session.commit()
+
+        created_route = Route.query.order_by(Route.created_at.desc()).first()
+        return created_route.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
 @route_routes.route('/<int:id>')
