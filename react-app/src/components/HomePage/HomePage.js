4f87fcbb3fdea1d85c6cb282f78b0d6b51ba6cbe
tyler <dtctorrero@icloud.com>
2022-11-28 18:57:40 -0800

    merge to dev

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 
 function HomePage() {
     const user = useSelector(state => state.session.user)
-    console.log('user=================', user.friends)
+    
   return (
     <div>
 
